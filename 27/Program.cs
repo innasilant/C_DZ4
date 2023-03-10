dd7b2a84abaa9688f1b4ab380a2f239d1759a3c0
«innasilant» <inna.palyova@mail.ru>
2023-03-10 14:05:26 +0300

    Added all

---
+++ 27/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int Result = 0; 
+
+for (int i = 0; i<=a; i++)
+{
+Result = Result + a%10;
+a = a/10; 
+}
+
+Console.WriteLine(Result);
